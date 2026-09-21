63b597c7d8a36ff8b0e08cfbeefe05783d1a3f89
Youmbi Bovan <youmbincbovan@gmail.com>
2026-09-21 22:25:43 +0100

    Revert "feat: Adding Addition and Substraction functions"
    This reverts commit 198ebf94f7dc9db028010fd4a3bc4009a930b6e7.

--- include/math.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-float add(float a, float b);
-float substract(float a, float b);
--- src/math.cpp
+++
@@ -1,4 +0,0 @@
-#include "math.h"
-
-float add(float a, float b) { return a + b; }
-float substract(float a, float b) { return a - b; }
