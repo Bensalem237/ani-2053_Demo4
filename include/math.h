198ebf94f7dc9db028010fd4a3bc4009a930b6e7
Youmbi Bovan <youmbincbovan@gmail.com>
2026-09-21 22:23:51 +0100

    feat: Adding Addition and Substraction functions

---
+++ include/math.h
@@ -0,0 +1,4 @@
+#pragma once
+
+float add(float a, float b);
+float substract(float a, float b);
---
+++ src/math.cpp
@@ -0,0 +1,4 @@
+#include "math.h"
+
+float add(float a, float b) { return a + b; }
+float substract(float a, float b) { return a - b; }
