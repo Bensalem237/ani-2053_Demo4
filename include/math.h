d833d7afef5855d4df0b5a5cbddc44056ee601ec
Youmbi Bovan <youmbincbovan@gmail.com>
2026-09-21 23:28:33 +0100

    Adding Multiplication and Division funtions

--- include/math.h
+++ include/math.h
@@ -2,3 +2,6 @@
 
 float add(float a, float b);
 float substract(float a, float b);
+
+float multiply(float a, float b);
+float divide(float a, float b);
--- src/math.cpp
+++ src/math.cpp
@@ -2,3 +2,9 @@
 
 float add(float a, float b) { return a + b; }
 float substract(float a, float b) { return a - b; }
+
+float multiply(float a, float b) { return a * b; }
+float divide(float a, float b) { 
+    if (b < 0) return 0;
+    return a / b; 
+}
