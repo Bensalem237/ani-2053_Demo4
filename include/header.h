b6b28fb47b42cc33131d3ecdd05b5af24dac3fff
Beling <julienbeling2007@gmail.com>
2026-09-21 23:00:39 +0100

    definition du discriminant et de la lecture

--- include/header.h
+++ include/header.h
@@ -1,4 +1,8 @@
 #ifndef HEADER_H
 #define HEADER_H
 
+float calc_delta(float a, float b, float c); // fonction de calcul du discriminant
+
+void lire_abc(float& a, float& b, float& c); // fonction de lecture
+
 #endif
