ed9eed007de37e9aaff57c76ce7b980da99ab2f3
Ben-salem <emmanuel.eponse@facsciences-uy1.cm>
2026-09-21 23:12:45 +0100

    feature: cree la fonction principale du resolveur d'equations quadratiques
    Le code principal a ete cree et les bibliotheques ont etes liees correctement

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,20 @@
 #include <iostream>
+#include <header.h>
+#include <cmath>
 
 int main() {
+    float a, b, c; // Les coefficients de chque terme de l'équation
+    float rac_1, rac_2;
+    float delta;
+    lire_abc(a, b, c);
+    delta = calc_delta(a, b, c);
+    // Calculer les racines de l'équation
+    if (delta < 0) {
+        std::cout << "L'équation n'a pas de racines réelles." << std::endl;
+    } else {
+        rac_1 = (-b + std::sqrt(delta)) / (2 * a);
+        rac_2 = (-b - std::sqrt(delta)) / (2 * a);
+        std::cout << "Les racines de l'équation sont : " << rac_1 << " et " << rac_2 << std::endl;
+    }
     return 0;
 }
